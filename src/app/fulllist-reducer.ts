4cae03674aeee5750f3cf1da9619d850d6a32b5a
Hakan Hurriyet <hakan@brewww.com>
2017-09-06 01:36:39 +0300

    ready to create second redux store file

---
+++ src/app/fulllist-reducer.ts
@@ -0,0 +1,14 @@
+
+import { Action } from '@ngrx/store';
+import { Observable } from 'rxjs/Rx';
+
+
+export interface State {
+    fulllist: Object
+}
+
+const initialState: State = {
+    fulllist:{}
+}
+
+
