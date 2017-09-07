0e46b4c2f5a0c9427b804c41655dbdb947defc9d
Hakan Hurriyet <hakan@brewww.com>
2017-09-08 01:59:33 +0300

    new reducer added

--- src/app/fulllist-actions.ts
+++ src/app/fulllist-actions.ts
@@ -1,2 +1,32 @@
-import { Action } from '@ngrx/store';
 
+import { Action } from '@ngrx/store';
+import { Iitem } from './model/iitem';
+
+
+export const ADD_ITEM = 'ADD_ITEM';
+export const CREATE_ITEM = 'CREATE_ITEM';
+export const UPDATE_ITEM = 'UPDATE_ITEM';
+export const DELETE_ITEM = 'DELETE_ITEM';
+
+export class AddItemAction implements Action {
+    readonly type = ADD_ITEM;
+    constructor(public payload: any ) {}
+}
+
+export class CreateItemAction implements Action {
+    readonly type = CREATE_ITEM;
+    constructor(public payload: any ){}
+}
+
+export class UpdateItemAction implements Action {
+    readonly type = UPDATE_ITEM;
+    constructor(public payload: any ) {}
+}
+
+export class DeleteItemAction implements Action  {
+    readonly type = DELETE_ITEM;
+    constructor( public payload: any ){}
+}
+
+export type Actions = AddItemAction | CreateItemAction | UpdateItemAction | DeleteItemAction;
+ 
--- src/app/fulllist-reducer.ts
+++ src/app/fulllist-reducer.ts
@@ -1,14 +1,34 @@
 
+
 import { Action } from '@ngrx/store';
 import { Observable } from 'rxjs/Rx';
 
+import * as fulllist from './fulllist-actions';
 
 export interface State {
-    fulllist: Object
-}
+  pay: string
+};
 
-const initialState: State = {
-    fulllist:{}
+export const initialState: State = {
+  pay:" "
+};
+
+export function reducer (state = initialState, action: fulllist.Actions): State {
+    switch(action.type) {
+    case fulllist.ADD_ITEM:
+        const items = action.payload
+        return Object.assign({},state,{
+            pay:'Zartongo'
+        })
+    case fulllist.CREATE_ITEM:
+       
+    case fulllist.DELETE_ITEM:
+       
+    case fulllist.UPDATE_ITEM:
+      
+    default:
+        return state;    
+    }
 }
 
 
